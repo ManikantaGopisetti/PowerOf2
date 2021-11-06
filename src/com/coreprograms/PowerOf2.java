6c73598415ba59b5f1c84ff943c7a2a32b62d9f4
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-06 23:59:45 +0530

    added n value by user input

---
+++ src/com/coreprograms/PowerOf2.java
@@ -0,0 +1,14 @@
+package com.coreprograms;
+import java.util.Scanner;
+
+public class PowerOf2 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("enter n value = ");
+		int n = scan.nextInt();
+		System.out.println("entered value is = "+n);
+	}
+	
+
+}
