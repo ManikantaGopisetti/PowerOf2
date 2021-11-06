706cdc6aa1ac6177fbb0407a4c777b02a3ef1484
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-07 00:14:32 +0530

    prnting power table of 2 upto n

--- src/com/coreprograms/PowerOf2.java
+++ src/com/coreprograms/PowerOf2.java
@@ -7,7 +7,18 @@ public class PowerOf2 {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("enter n value = ");
 		int n = scan.nextInt();
-		System.out.println("entered value is = "+n);
+		
+		if (n>0&&n<31) {	//Only works if 0 <= N < 31 since 2^31 overflows an int
+			int i=0;
+			for(i=0;i<=n;i++) {
+				int power = (int)Math.pow(2, i);
+				System.out.println("2^"+i+"="+power);
+			}
+		}
+		else {
+			System.out.println("enter value less than 31 as the given value exceeds int range");
+		}
+		
 	}
 	
 
